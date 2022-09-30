b50b31f4bd221c660b24180e4c2678fe6cf0edc2
Tettu0530 <89886910+Tettu0530@users.noreply.github.com>
2022-09-30 16:14:40 +0900

    Fixed messages

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ res = subprocess.check_output(command).decode("utf-8")
 
 res = res.replace("UUID", "")
 hwid = res.strip()
-print(hwid)
+print(f"This i your HWID : {hwid}")
 
 with open("hwid.txt", "w", encoding = "utf-8") as f:
     f.write(hwid)
+    os.system("PAUSE")
