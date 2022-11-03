f6a8b61db4cf6311a125c5f3141324b40ea9773e
Tettu0530 <89886910+Tettu0530@users.noreply.github.com>
2022-11-03 09:34:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ res = subprocess.check_output(command).decode("utf-8")
 
 res = res.replace("UUID", "")
 hwid = res.strip()
-print(f"This i your HWID : {hwid}")
+print(f"This is your HWID : {hwid}")
 
 with open("hwid.txt", "w", encoding = "utf-8") as f:
     f.write(hwid)
